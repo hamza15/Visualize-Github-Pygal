b27c787928cd671453389eacd244b0a00f3e483a
Sardar Hamza Naqi <sardan5@vt.edu>
2018-08-06 23:31:39 -0400

    Create python_repos.py

---
+++ python_repos.py
@@ -0,0 +1,32 @@
+import requests
+import pygal
+
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+#Make an API call and store the respone
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print"Status code:", r.status_code
+
+#Store API response in a variable
+response_dict = r.json()
+print "Total repositories:", response_dict['total_count']
+
+#Explore information about the repositories.
+repo_dicts = response_dict['items']
+print"Repositories returned:", len(repo_dicts)
+
+names, stars = [], []
+#Examine the first repository
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+	names.append(repo_dict['name'])
+ 	stars.append(repo_dict['stargazers_count'])
+
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg') 	
